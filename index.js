a5889e1666a0e4efc53102db2d9fe75cf66b2a9b
Kevin Arias <kariasduarte@gmail.com>
2023-01-25 16:18:54 -0500

    servidor express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hola")
+})
+
+app.listen(port, () => {
+  console.log("mi puerto es " + port)
+})
